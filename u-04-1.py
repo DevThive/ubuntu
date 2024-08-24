2cfc72d9aeb40f8ed5e892bb34da9c6623b33560
Devthive <thive8564@gmail.com>
2024-08-25 00:39:45 +0900

    Update u-04-1.py

--- u-04-1.py
+++ u-04-1.py
@@ -1,7 +1,6 @@
 import os
 
 def check_root_password_field(file_path):
-     
     try:
         with open(file_path, 'r') as file:
             for line in file:
@@ -15,7 +14,7 @@ def check_root_password_field(file_path):
                         print("---------------------------------")
                         print("root의 두 번째 필드가 'x'가 아닙니다.")
                         print("---------------------------------")
-                     break
+                    break
             else:
                 print("root 사용자 정보를 찾을 수 없습니다.")
     
@@ -40,17 +39,17 @@ def change_passwd_permissions():
 
 def check_password_file_rule(file):
     # 파일 권한 정보를 가져옴
-    result = os.popen(f"ls -al {file}").read()
+    result = os.popen(f"ls -al {file}").read().strip()
     
     # 권한 부분 추출
-    permissions = result.split()[0]
+    permissions = result.split()[0][1:]  # 첫 문자 '-'를 제외하고 권한 부분만 가져옴
 
     # 권한 확인
     if permissions == 'rw-r-----':
         print(f"{file} 파일의 권한이 rw-r-----으로 설정되어 있습니다.")
     else:
         print(f"{file} 파일의 권한이 rw-r-----이 아닙니다. 현재 권한: {permissions}")
-        change_password_permissions()
+        change_passwd_permissions()
 
 def check_passwd_file():
     file_path = '/etc/passwd'
@@ -62,10 +61,5 @@ def check_passwd_file():
     else:
         print(f"{file_path} 파일이 존재하지 않습니다.")
 
-
 # 함수 실행
-
-
-
-# 파일 존재 확인 실행
 check_passwd_file()
