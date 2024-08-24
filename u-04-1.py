7c6ebb92737bd25f1b70b8577e53c379cd79cb0d
Devthive <thive8564@gmail.com>
2024-08-24 17:53:10 +0900

    Update u-04-1.py

--- u-04-1.py
+++ u-04-1.py
@@ -1,30 +1,7 @@
 import os
 
-def check_password_file_rule(file):
-    # 파일 권한 정보를 가져옴
-    result = os.popen(f"ls -al {file}").read()
-    
-    # 권한 부분 추출
-    permissions = result.split()[0]
-
-    # 권한 확인
-    if permissions == 'rw-r-----':
-        print(f"{file} 파일의 권한이 rw-r-----으로 설정되어 있습니다.")
-    else:
-        print(f"{file} 파일의 권한이 rw-r-----이 아닙니다. 현재 권한: {permissions}")
-
-def check_passwd_file():
-    file_path = '/etc/passwd'
-    
-    if os.path.exists(file_path):
-        print(f"{file_path} 파일이 존재합니다.")
-        check_password_file_rule(file_path)
-    else:
-        print(f"{file_path} 파일이 존재하지 않습니다.")
-
-def check_root_password_field():
-    file_path = '/etc/passwd'
-    
+def check_root_password_field(file_path):
+     
     try:
         with open(file_path, 'r') as file:
             for line in file:
@@ -47,8 +24,33 @@ def check_root_password_field():
         print(f"오류 발생: {e}")
         print("---------------------------------")
 
+
+def check_password_file_rule(file):
+    # 파일 권한 정보를 가져옴
+    result = os.popen(f"ls -al {file}").read()
+    
+    # 권한 부분 추출
+    permissions = result.split()[0]
+
+    # 권한 확인
+    if permissions == 'rw-r-----':
+        print(f"{file} 파일의 권한이 rw-r-----으로 설정되어 있습니다.")
+    else:
+        print(f"{file} 파일의 권한이 rw-r-----이 아닙니다. 현재 권한: {permissions}")
+
+def check_passwd_file():
+    file_path = '/etc/passwd'
+    
+    if os.path.exists(file_path):
+        print(f"{file_path} 파일이 존재합니다.")
+        check_password_file_rule(file_path)
+        check_root_password_field(file_path)
+    else:
+        print(f"{file_path} 파일이 존재하지 않습니다.")
+
+
 # 함수 실행
-check_root_password_field()
+
 
 
 # 파일 존재 확인 실행
